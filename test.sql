31542482cd710c1fbe1d0b849453114fd7ef2b4d
Alexandre DELORME <alexandre.delorme@viacesi.fr>
2021-12-01 12:53:52 +0100

    test.sql

--- test.sql
+++ test.sql
@@ -1,4 +1,4 @@
-﻿--select A.Reference, Nom, QuantiteStock, PrixProduit, SeuilReapprovisionnement, NomTaxe, PourcentageTaxe from Taxer Tr join Article as A on Tr.Reference = A.Reference join Taxe as Ta on Tr.IDTaxe = Ta.IDTaxe
-
---select * from Taxe
-
+﻿select MONTH(DatePaiement) as Mois, sum(MontantPaiement) as [Chiffre d affaire]
+from Paiement
+group by MONTH(DatePaiement)
+having MONTH(DatePaiement) = 5
